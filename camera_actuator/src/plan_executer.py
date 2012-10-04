6f8d54c9bfd93d18ce3eed4a9cd1bf22c2fd3ee1
adamnilsson <adam.nilss@gmail.com>
2012-10-04 16:02:30 -0700

    minor

--- camera_actuator/src/plan_executer.py
+++ camera_actuator/src/plan_executer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from camera_actuator.msg import IntArray
 from camera_actuator.srv import voidService
+from camera_actuator.srv import planCommand
 import roslib 
 import rospy
 import sys
@@ -16,22 +17,29 @@ class PlanExecuter:
     def planCallback(self, msg):
         self.plan = msg.data
         
+    def handle_execute_plan(self, req):
+        
+        self.executePlan(req.plan)        
+        
     def getPlan(self):
         return self.plan
-        
+
+    def executeCommand(self, index):
+        this_cmd = self.command_list[cmd_index]
+            
+        print 'Sending command:     ',this_cmd
+        self.cmd_publisher.publish(get_msg(this_cmd))
+        try:
+            cameraBusy = rospy.ServiceProxy('/logitech_cam/cameraBusy', voidService)
+            cameraBusy(1)
+            print('Device is ready for next command')
+        except rospy.ServiceException, e:
+            print('Unexpected Error: rospy.ServiceException')
+            
     def executePlan(self):
         print('Executing plan: ', self.plan )
         for cmd_index in self.getPlan():
-            this_cmd = self.command_list[cmd_index]
-            
-            print 'Sending command:     ',this_cmd
-            self.cmd_publisher.publish(get_msg(this_cmd))
-            try:
-                cameraBusy = rospy.ServiceProxy('/logitech_cam/cameraBusy', voidService)
-                cameraBusy(1)
-                print('Device is ready for next command')
-            except rospy.ServiceException, e:
-                print('Unexpected Error: rospy.ServiceException')
+            executeCommand(cmd_index)
         
     def main(self, args):
         print('main()')
@@ -40,6 +48,8 @@ class PlanExecuter:
         rospy.init_node('plan_executer', anonymous=True)
         self.cmd_publisher = rospy.Publisher('/logitech_cam/camera_instr', IntArray)
         
+        s = rospy.Service('/logitech_camera/executePlan', planCommand, self.handle_execute_plan)
+        
         sys.stdin.readline()
 
         self.executePlan()
@@ -47,6 +57,7 @@ class PlanExecuter:
         while not rospy.is_shutdown():        
             rospy.spin()
             
+
             
 def get_msg(intarray):
     msg = IntArray()
